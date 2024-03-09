03f7f21dfbeddf119ca8eea95aeee570b46e9b23
juandi00 <161092941+juandi00@users.noreply.github.com>
2024-03-08 21:14:49 -0500

    Add files via upload

---
+++ metros, millasw.py
@@ -0,0 +1,7 @@
+
+metros = float(input("Ingrese la distancia en metros: "))
+
+
+millas = metros * 0.041631857
+
+print(f"{metros} metros equivalen a {millas} millas")
---
+++ salario.py
@@ -0,0 +1,10 @@
+
+salario_minimo_actual = 908526
+
+
+incremento_porcentaje = 4.2 / 100
+
+
+nuevo_salario_minimo = salario_minimo_actual * (1 + incremento_porcentaje)
+
+print(f"El nuevo salario mínimo para el siguiente año será de: {nuevo_salario_minimo:.2f} pesos")
